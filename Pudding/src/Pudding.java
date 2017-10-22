020369bdabc5035e4a62aeb93fa8834f12a767ae
David Bowen <dhbowen42@gmail.com>
2017-10-22 11:36:44 -0500

    Reads in and can print a .doc file. Needs to add file input of .docx later

--- Pudding/src/Pudding.java
+++ Pudding/src/Pudding.java
@@ -1,6 +1,40 @@
+import java.io.*;
+import java.util.List;
+
+import org.apache.poi.hwpf.HWPFDocument;
+import org.apache.poi.hwpf.extractor.WordExtractor;
+import org.apache.poi.openxml4j.opc.OPCPackage;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+
+
 public class Pudding {
+
+    public static void readDocFile(String fileName) {
+
+        try {
+            File file = new File(fileName);
+            FileInputStream fis = new FileInputStream(file.getAbsolutePath());
+
+            HWPFDocument doc = new HWPFDocument(fis);
+
+            WordExtractor we = new WordExtractor(doc);
+
+            String[] paragraphs = we.getParagraphText();
+
+            System.out.println("Total no of paragraph "+paragraphs.length);
+            for (String para : paragraphs) {
+                System.out.println(para.toString());
+            }
+            fis.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        readDocFile("paper.doc");
+
     }
 }
-
